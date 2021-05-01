01f22f84b4d1c356a24161ea3680cf2b81d8df98
Gourav Banerjee <39580718+C713Gb@users.noreply.github.com>
2021-05-01 07:38:17 +0530

    initial commit

---
+++ app/src/main/java/com/banerjee/mvvmcookbook/view/actvities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.banerjee.mvvmcookbook.view.actvities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.banerjee.mvvmcookbook.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
